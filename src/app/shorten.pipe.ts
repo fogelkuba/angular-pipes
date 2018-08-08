e029cd84f457d801630ba8a309a6774882d3fcc4
fogelkuba <fogelkuba@gmail.com>
2018-08-08 21:23:57 +0200

    fix

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -5,6 +5,9 @@ import {Pipe, PipeTransform} from "@angular/core";
 })
 export class ShortenPipe implements PipeTransform {
   transform(value: any) {
-    return value.substr(0, 10);
+    if (value.length > 10) {
+      return value.substr(0, 9) + ' ...';
+    }
+    return value;
   }
 }
